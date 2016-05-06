40380f235e61f626fe3c39492bb4a804a472f261
Sahil Dua <sahildua2305@gmail.com>
2016-05-07 04:24:54 +0530

    Check for byzanz package, install if absent

--- byzanz-record-window.sh
+++ byzanz-record-window.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
 
+# Check for byzanz and install it if not installed already
+if [ $(dpkg-query -W -f='${Status}' byzanz 2>/dev/null | grep -c "ok installed") == "0" ]; then
+	echo "'byzanz' is not installed."
+	echo "Trying to install it now..."
+	echo
+	echo
+	if sudo apt-get --assume-yes install byzanz; then
+		echo "Installed 'byzanz' successfully."
+		echo
+	else
+		echo "Unable to install byzanz. Please try again."
+		exit 1
+	fi
+fi
+
 if [ $# -gt 0 ]; then
 	DELAY="$1"
 else
@@ -15,6 +30,11 @@ else
     D="--duration=10 /tmp/recorded.gif"
 fi
 
+echo
+echo "Go to the window (alt+tab) you want to capture and click on it."
+echo "Waiting for you to click on a window to record..."
+echo
+
 # Ask for selecting the window
 XWININFO=$(xwininfo)
 read X <<< $(awk -F: '/Absolute upper-left X/{print $2}' <<< "$XWININFO")
@@ -23,6 +43,7 @@ read W <<< $(awk -F: '/Width/{print $2}' <<< "$XWININFO")
 read H <<< $(awk -F: '/Height/{print $2}' <<< "$XWININFO")
 
 echo Delaying $DELAY seconds. After that, byzanz will start recording
+
 for (( i=$DELAY; i>0; --i )) ; do
     echo $i
     sleep 1
