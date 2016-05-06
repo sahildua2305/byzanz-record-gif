b41a0a1f28d34c41f2af12c52e3fbef9d0aaa1b4
Sahil Dua <sahildua2305@gmail.com>
2016-05-07 02:49:44 +0530

    Added more control to command line arguments

--- byzanz-record-window.sh
+++ byzanz-record-window.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-# Delay before starting
-DELAY=10
 
-# Sound notification to let one know when recording is about to start (and ends)
-beep() {
-    paplay /usr/share/sounds/KDE-Im-Irc-Event.ogg &
-}
+if [ $# -gt 0 ]; then
+	DELAY="$1"
+else
+	DELAY=10
+fi
 
 # Duration and output file
 if [ $# -gt 0 ]; then
-    D="--duration=$@"
+    D="--duration=$2 $3"
 else
     echo Default recording duration 10s to /tmp/recorded.gif
     D="--duration=10 /tmp/recorded.gif"
 fi
+
+# Ask for selecting the window
 XWININFO=$(xwininfo)
 read X <<< $(awk -F: '/Absolute upper-left X/{print $2}' <<< "$XWININFO")
 read Y <<< $(awk -F: '/Absolute upper-left Y/{print $2}' <<< "$XWININFO")
 read W <<< $(awk -F: '/Width/{print $2}' <<< "$XWININFO")
 read H <<< $(awk -F: '/Height/{print $2}' <<< "$XWININFO")
 
-echo Delaying $DELAY seconds. After that, byzanz will start
+echo Delaying $DELAY seconds. After that, byzanz will start recording
 for (( i=$DELAY; i>0; --i )) ; do
     echo $i
     sleep 1
